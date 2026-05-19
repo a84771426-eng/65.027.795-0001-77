8a94046c1ee6a66e31a205d446689578fdcedc9c
a84771426-eng <a84771426@gmail.com>
2026-05-19 00:22:24 -0300

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,15 @@
+/**
+ * Dados da empresa.
+ * WhatsApp: apenas números com DDI (ex: 5511999999999).
+ */
+window.SITE_CONFIG = {
+  companyName: "CANPAR",
+  legalName: "CANPAR DESPACHOS ADUANEIROS LTDA",
+  whatsapp: "551997903228",
+  whatsappDisplay: "(19) 9790-3228",
+  phone: "(19) 9790-3228",
+  email: "andreia@canpar.com.br",
+  address: "Rua Rodrigues Alves, 657",
+  location: "Rua Rodrigues Alves, 657 - Campinas - SP, CEP 13020-400",
+  cnpj: "65.027.795/0001-77",
+};
